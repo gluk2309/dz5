13879573c95708cc86ca91d05e2bf9aaf9b5e5a6
gluk2309 <gluk2309@mail.ru>
2023-07-25 12:00:03 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public int calculate(int weight,double height) {
+       double bmi = weight / Math.pow(height, 2);
+       return (int) bmi;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        int weight = 98;
+        double height = 1.87;
+        int index = service.calculate(weight,height);
+        System.out.println(index);
+    }
+}
